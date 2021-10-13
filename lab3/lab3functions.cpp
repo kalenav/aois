f5ae91b25efd2847c7fee5d03c05f56480af116a
kalenav <konstantin.loktev.02@mail.ru>
2021-10-13 11:27:10 +0300

    склеивание конституэнт получилось, судя по всему

--- lab3/lab3functions.cpp
+++ lab3/lab3functions.cpp
@@ -15,6 +15,7 @@ bool isSKNF(string input, int argumentsQuantity)
 		if (input[i] != ' ' && input[i] != '!' && input[i] != 'x' && input[i] != '*') variablesInCurrentParenthesesSet[(int)(input[i] - '0') - 1]++;
 		if (input[i] == ')')
 		{
+			if (i < size(input) - 2 && input[i + 2] == '+') return false;
 			for (int j = 0; j < argumentsQuantity; j++) if (variablesInCurrentParenthesesSet[j] != 1) return false;
 		}
 	}
@@ -33,6 +34,7 @@ bool isSDNF(string input, int argumentsQuantity)
 		if (input[i] != ' ' && input[i] != '!' && input[i] != 'x' && input[i] != '+') variablesInCurrentParenthesesSet[(int)(input[i] - '0') - 1]++;
 		if (input[i] == ')')
 		{
+			if (i < size(input) - 2 && input[i + 2] == '*') return false;
 			for (int j = 0; j < argumentsQuantity; j++) if (variablesInCurrentParenthesesSet[j] != 1) return false;
 		}
 	}
@@ -41,10 +43,28 @@ bool isSDNF(string input, int argumentsQuantity)
 
 bool areNeighboring(string left, string right)
 {
-	int negatedLeft = 0, negatedRight = 0;
-	for (int i = 0; i < size(left); i++) if (left[i] == '!') negatedLeft++;
-	for (int i = 0; i < size(right); i++) if (right[i] == '!') negatedRight++;
-	if (abs(negatedLeft - negatedRight) == 1) return true;
+	int differentArguments = 0;
+	for (int leftIndex = 0; leftIndex < size(left) - 2; leftIndex++)
+	{
+		if (left[leftIndex] == '!')
+		{
+			for (int rightIndex = 1; rightIndex < size(right); rightIndex++)
+			{
+				if (right[rightIndex] == left[leftIndex + 2] && (rightIndex - 2 == -1 || right[rightIndex - 2] == ' ')) differentArguments++;
+			}
+		}
+	}
+	for (int rightIndex = 0; rightIndex < size(right) - 2; rightIndex++)
+	{
+		if (right[rightIndex] == '!')
+		{
+			for (int leftIndex = 1; leftIndex < size(left); leftIndex++)
+			{
+				if (left[leftIndex] == right[rightIndex + 2] && (leftIndex - 2 == -1 || left[leftIndex - 2] == ' ')) differentArguments++;
+			}
+		}
+	}
+	if (differentArguments == 1) return true;
 	else return false;
 }
 
@@ -72,28 +92,28 @@ string concatenateNeighboring(string left, string right)
 			currentPossibleExtraTerm += left[leftIndex + 1];
 			if (currentPossibleExtraTerm[0] == '!')
 			{
-				for (int rightIndex = 0; rightIndex < size(right) - 2; rightIndex++)
+				for (int rightIndex = 0; rightIndex < size(right) - 1; rightIndex++)
 				{
-					if (right[rightIndex] != '!' && right[rightIndex + 1] == 'x' && right[rightIndex + 2] == currentPossibleExtraTerm[2])
+					if (right[rightIndex] == 'x' && right[rightIndex + 1] == currentPossibleExtraTerm[2] && (rightIndex == 0 || right[rightIndex - 1] != '!'))
 					{
 						extraTermIndex = leftIndex - 1;
 						breakUpperCycle = true;
 						break;
 					}
-					if (rightIndex == size(right) - 3) currentPossibleExtraTerm = "";
+					if (rightIndex == size(right) - 2) currentPossibleExtraTerm = "";
 				}
 			}
 			else
 			{
-				for (int rightIndex = 0; rightIndex < size(right) - 2; rightIndex++)
+				for (int rightIndex = 0; rightIndex < size(right) - 1; rightIndex++)
 				{
-					if (right[rightIndex] == '!' && right[rightIndex + 1] == 'x' && right[rightIndex + 2] == currentPossibleExtraTerm[1])
+					if (right[rightIndex] == 'x' && right[rightIndex + 1] == currentPossibleExtraTerm[1] && (rightIndex == 0 || right[rightIndex - 1] == '!'))
 					{
 						extraTermIndex = leftIndex;
 						breakUpperCycle = true;
 						break;
 					}
-					if (rightIndex == size(right) - 3) currentPossibleExtraTerm = "";
+					if (rightIndex == size(right) - 2) currentPossibleExtraTerm = "";
 				}
 			}
 		}
@@ -131,7 +151,50 @@ string concatenateNeighboring(string left, string right)
 
 string concatenateStage1(string input)
 {
-	return input;
+	string result;
+	string* constituents;
+	int arrSize = 0, argumentsQuantity = 0;
+	bool readingSymbols = false;
+	for (int i = 0; i < size(input); i++)
+	{
+		if (input[i] == '(') arrSize++;
+		if (input[i] != ' ' && input[i] != '!' && input[i] !=  '*' && input[i] != '+' && input[i] != 'x' && (int)(input[i] - '0') > argumentsQuantity) argumentsQuantity = (int)(input[i] - '0');
+	}
+	constituents = new string [arrSize];
+	int currConstituent = -1;
+	for (int i = 0; i < size(input); i++)
+	{
+		if (input[i] == ')')
+		{
+			readingSymbols = false;
+		}
+		if (readingSymbols) constituents[currConstituent] += input[i];
+		if (input[i] == '(')
+		{
+			readingSymbols = true;
+			currConstituent++;
+		}
+	}
+	string left, right;
+	string connector = (isSDNF(input, argumentsQuantity)) ? " + " : " * ";
+	bool firstIteration = true;
+	for (int currLeftConstituent = 0; currLeftConstituent < arrSize; currLeftConstituent++)
+	{
+		left = constituents[currLeftConstituent];
+		for (int currRightConstituent = currLeftConstituent + 1; currRightConstituent < arrSize; currRightConstituent++)
+		{
+			right = constituents[currRightConstituent];
+			if (areNeighboring(left, right))
+			{
+				if (!firstIteration) result += connector;
+				else firstIteration = false;
+				result += '(';
+				result += concatenateNeighboring(left, right);
+				result += ')';
+			}
+		}
+	}
+	return result;
 }
 
 string concatenateStage2(string input)
